abf4e5bfc9b38a1ba5091585acd2d155cd99719a
Jay <jiyoung26091212@gmail.com>
2022-05-15 21:52:07 +0100

    feat : get recipes excluding ones with exclude_ingredients

--- controllers/recipe.controller.js
+++ controllers/recipe.controller.js
@@ -1,9 +1,11 @@
 const { selectRecipes } = require('../models/recipe.model.js');
 
 function getRecipes(req, res, next) {
-	selectRecipes()
+  const { exclude_ingredients } = req.query;
+
+	selectRecipes(exclude_ingredients)
 		.then((recipes) => {
-			res.status(200).send({ recipes });
+			res.status(200).send({ recipes: recipes });
 		})
 		.catch((err) => {
 			console.log(err);
--- models/recipe.model.js
+++ models/recipe.model.js
@@ -1,8 +1,21 @@
 const fs = require('fs/promises');
 const db = './data/data.json';
 
-exports.selectRecipes = async () => {
-  const data = await fs.readFile(db, 'utf8');
-	const recipes = JSON.parse(data);
+exports.selectRecipes = async (exclude_ingredients) => {
+	const data = await fs.readFile(db, 'utf8');
+	let recipes = JSON.parse(data);
+
+	if (exclude_ingredients) {
+		const filtered = recipes.filter((recipe) => {
+			let counter = 0;
+			recipe.ingredients.forEach((ingredient) => {
+				if (ingredient.name !== exclude_ingredients) counter++;
+			});
+			if (counter === recipe.ingredients.length) {
+				return recipe;
+			}
+		});
+		recipes = filtered;
+	}
 	return recipes;
 };
