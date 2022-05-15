1a852de72f87561016813a6e28a756ea0a9280bd
Jay <jiyoung26091212@gmail.com>
2022-05-15 19:15:43 +0100

    Test : Add test to check properties in recipe object

--- test/api.test.js
+++ test/api.test.js
@@ -4,11 +4,30 @@ const server = require('../server');
 const request = supertest(server);
 
 test('/api', async () => {
-	await request.get('/api').expect(200);
+	const { body } = await request.get('/api').expect(200);
+	expect(body.message).toBe('ok');
 });
 
 describe('Get /api/recipes', () => {
-	test('200 respond with all reciepes', async () => {
+	test('200 respond with all recipes', async () => {
 		const { body } = await request.get('/api/recipes').expect(200);
+		expect(body.recipes.length).toBe(100);
+		expect(body.recipes).toBeInstanceOf(Object);
+		body.recipes.forEach((recipe) => {
+			expect(recipe).toMatchObject({
+				id: expect.any(String),
+				imageUrl: expect.any(String),
+				instructions: expect.any(String),
+				ingredients: expect.any(Array),
+			});
+			expect(
+				recipe.ingredients.forEach((ingredient) => {
+					expect(ingredient).toMatchObject({
+						name: expect.any(String),
+						grams: expect.any(Number),
+					});
+				})
+			);
+		});
 	});
 });
