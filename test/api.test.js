24edf330ef28aea466990793441bcdcb7b86d395
Jay <jiyoung26091212@gmail.com>
2022-05-15 21:51:28 +0100

    test : /api/recipes?exclude_ingredients - get recipes excluding the ones having exclude_ingredients

--- test/api.test.js
+++ test/api.test.js
@@ -12,7 +12,7 @@ describe('Get /api/recipes', () => {
 	test('200 respond with all recipes', async () => {
 		const { body } = await request.get('/api/recipes').expect(200);
 		expect(body.recipes.length).toBe(100);
-		expect(body.recipes).toBeInstanceOf(Object);
+		expect(body.recipes).toBeInstanceOf(Array);
 		body.recipes.forEach((recipe) => {
 			expect(recipe).toMatchObject({
 				id: expect.any(String),
@@ -30,4 +30,13 @@ describe('Get /api/recipes', () => {
 			);
 		});
 	});
+
+	test('200 /api/recipes?exclude_ingredients=banana : respond with an array of objects excluding ones having banana as its ingredients', async () => {
+		const { body } = await request
+			.get('/api/recipes?exclude_ingredients=banana')
+			.expect(200);
+		expect(body.recipes).toBeInstanceOf(Array);
+		expect(body.recipes.length).toBeLessThanOrEqual(90);
+	});
+  
 });
